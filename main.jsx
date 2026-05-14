21af975837228185e43af97f5ceaa3c5ee298cc2
Kauã Pereira Melo <kauap9040@gmail.com>
2026-05-14 09:04:53 -0300

    fix: corrige importacao do componente

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './sistema-marketing.jsx' // Verifique se o nome do arquivo está idêntico
+import App from './App.jsx' // Verifique se o nome do arquivo está idêntico
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
